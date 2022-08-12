4764a62c5cfe455c04dbc3d6e8219059bdd39dd0
wajih942 <73997820+wajih942@users.noreply.github.com>
2022-08-12 16:06:51 +0200

    commit

---
+++ hardhat.config.ts
@@ -0,0 +1,39 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+import "@typechain/hardhat"
+import "@nomiclabs/hardhat-ethers"
+import "hardhat-deploy"
+import { HardhatUserConfig } from "hardhat/config"
+const config: HardhatUserConfig = {
+  defaultNetwork: "hardhat",
+  networks: {
+    hardhat: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+    localhost: {
+      chainId: 31337,
+      allowUnlimitedContractSize: true
+    },
+  },
+  solidity: {
+    version: "0.8.8",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
+      },
+    },
+  },
+  namedAccounts: {
+    deployer: {
+      default: 0, // here this will by default take the first account as deployer
+      1: 0, // similarly on mainnet it will take the first account as deployer. Note though that depending on how hardhat network are configured, the account 0 on one network can be different than on another
+    },
+  },
+  mocha: {
+    timeout: 200000, // 200 seconds max for running tests
+  },
+}
+
+export default config
+
